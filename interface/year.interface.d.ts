16107808973672566194b840ab27a5563b6582b3
heroing110 <heroing110@163.com>
2017-02-02 09:56:45 +0800

    year实体里包含cityId和userId 后台判断新增还是修改，根据year实体是否传id

--- interface/year.interface.d.ts
+++ interface/year.interface.d.ts
@@ -1,17 +1,17 @@
-// Created by baihuibo on 2017/1/31.
+ // Created by baihuibo on 2017/1/31.
 import {Year} from "../src/entity/year";
 
 // path = '/api/year'
 export interface YearServiceInterface {
-    // GET
-    queryAll(userId: string, permission: string): Year[]
+    // GET 通过cityId和userId索引，1：只传cityId、不传userId表示查本市数据; 2:都不传，查询所有数据；
+    queryAll(cityId: string,userId: string): Year[]
 
     // GET path = '/detail'
     queryDetail(yearId: string): Year
 
     // POST path = '/insert'
-    insert(userId: string, year: Year): Year
+    insert(year: Year): Year
 
     // PUT path = '/update', params='yearId'
-    update(yearId: string, year: Year): Year
+    update(year: Year): Year
 }
