8839c6862c2e2aea3f3fab5c585a09a05a3c7352
baihuibo <baihuibo@hotmail.com>
2017-02-13 09:29:44 +0800

    新增用户相关接口

--- interface/users.interface.d.ts
+++ interface/users.interface.d.ts
@@ -5,7 +5,53 @@ import {UserInfo} from "../src/entity/user-info";
 // path = '/api/users'
 export interface UsersServiceInterface {
 
-    // POST  path = '/login'
-    login(username: string,
-          password: string): {login: boolean, user: UserInfo};
+  /**
+   * 用户登陆
+   * @param username
+   * @param password
+   *
+   * method = POST
+   * path = '/login'
+   */
+  login(username: string,
+        password: string): {login: boolean, user: UserInfo};
+
+  /**
+   * 查询全部用户
+   * @param userId 必须参数
+   * @param permission 必须参数
+   * @param params 可选，如果有查询参数，则根据参数过滤查询
+   *
+   * method = GET
+   * path = '/queryAll'
+   */
+  queryAll(userId: string, permission: string, params: Object): UserInfo[]
+
+  /**
+   * 新增用户
+   *
+   * method = PUT
+   * path = '/insert'
+   */
+  insert(): {/* 创建完成情况 */inserted: boolean}
+
+  /**
+   * 删除用户
+   * @param userId
+   *
+   * method = delete
+   * path = '/delete'
+   */
+  delete(userId: string): {/* 删除情况 */removed: boolean}
+
+  /**
+   * 修改用户信息
+   * @param userId
+   * @param user
+   *
+   * method = POST
+   * path = '/update'
+   * params = 'userId'
+   */
+  update(userId: string, user: UserInfo): {/* 更新完成情况 */updated: boolean}
 }
